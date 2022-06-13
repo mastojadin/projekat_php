2d13f0af14ade7582bf0fe7111ce7f8ca9375bfa
mastojadin <mastojadin@gmail.com>
2022-06-14 00:16:15 +0200

    ...

--- app/classes/My_data.php
+++ app/classes/My_data.php
@@ -13,22 +13,62 @@ class My_data {
 
     public function add() :int
     {
-        return 1;
+        $post = $_POST;
+        $category_id = $post['category_id'];
+        $amount = $post['amount'];
+        $date = date('Y-m-d H:i:s');
+
+        // validation
+
+        $query = "INSERT INTO my_data (category_id, amount, date) VALUES (:category_id, :amount, :date)";
+        $params = array(':category_id' => $category_id, ':amount' => $amount, ':date' => $date);
+        $res = DBHelper->do_my_query($query, $params);
+
+        return $this->list();
     }
 
     public function update() :int
     {
-        return 1;
+        $post = $_POST;
+        $my_data_id = $post['my_data_id'];
+        $category_id = $post['category_id'];
+        $amount = $post['amount'];
+        $date = $post['date'];
+
+        // validation
+
+        $query = "UPDATE my_data SET category_id = :category_id, amount = :amount, date = :date WHERE id = :my_data_id";
+        $params = array(':category_id' => $category_id, ':amount' => $amount, ':date' => $date, ':my_data_id' => $my_data_id);
+        $res = DBHelper->do_my_query($query, $params);
+
+        return $this->list();
     }
 
     public function delete() :int
     {
-        return 1;
+        $post = $_POST;
+        $my_data_id = $post['my_data_id'];
+
+        // validation
+        
+        $query = "DELETE FROM my_data WHERE id = :my_data_id";
+        $params = array(':my_data_id' => $my_data_id);
+
+        $res = DBHelper->do_my_query($query, $params);
+
+        return $this->list();
     }
 
     public function list() :array
     {
-        return [];
+        $date = date('Y-m-' . '01');
+        
+        $query = "SELECT id, category_id, amount, date FROM my_data WHERE date > :date";
+        $params = array(':date' => $date);
+
+        $res = DBHelper->do_my_query($query, $params);
+
+        return $res;
     }
 
     public function search() :array
